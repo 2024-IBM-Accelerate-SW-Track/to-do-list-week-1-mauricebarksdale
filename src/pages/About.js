c474c87c41880d481f6d57149b0585d73f026ef0
mauricebarksdale <momobarksdale@gmail.com>
2024-06-06 23:32:07 -0400

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,24 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              // Image goes here
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Maurice Barksdale</div>
+            <div className="brief_description">
+              // Details about you goes here
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
